435432144728d0a24ddd5e650b5021d4c094cee1
nobyn0312 <nobyn0312@gmail.com>
2024-06-18 21:38:56 +0900

    テスト

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const onClickAdd =()=>{
+  const inputText = document.getElementById('add-text').value
+  document.getElementById('add-text').value = ''
+const li =document.createElement('li')
+const div =document.createElement('div')
+div.className = 'list-row'
+const p =document.createElement('p')
+p.className ='todo-item'
+p.innerText = inputText
+
+div.appendChild(p)
+li.appendChild(div)
+console.log(li)
+}
+
+document.getElementById('add-button').addEventListener('click',onClickAdd)
